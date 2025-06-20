1c2744f455d78b13c5bb421f45d90a13c9afe41f
Batraz Zaritskiy <batik1315@gmail.com>
2025-06-20 03:16:00 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000; // Используйте переменную окружения для порта
+
+app.get('/', (req, res) => {
+  res.send('Hello from my custom Node.js app for {{CLIENT_NAME}}!');
+});
+
+app.listen(port, () => {
+  console.log(`Node.js app listening on port ${port}`);
+});
