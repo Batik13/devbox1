ffee822e3365625e7f1bfe414e8180e7ae35a3c2
Batraz Zaritskiy <batik1315@gmail.com>
2025-07-03 18:15:48 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000; // Используйте переменную окружения для порта
 
 app.get('/', (req, res) => {
-  res.send('Hello user');
+  res.send('Hello NoName');
 });
 
 app.listen(port, () => {
